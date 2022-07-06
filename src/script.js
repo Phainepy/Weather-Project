2ec40384e9eec48579ece1185d0b77c51a671e51
Phainepy <87214054+Phainepy@users.noreply.github.com>
2022-07-05 20:20:00 -0700

    Unit conversion for the Temperature F to C and vice versa is working now.

--- src/script.js
+++ src/script.js
@@ -1,60 +1,28 @@
-//Function handlePosition asks for Geolocation data from the user and then feeds it to showCurrentTemp`
-function handlePosition(position){
-  let latitude= position.coords.latitude;
-  let longitude= position.coords.longitude;
-  let units = "metric";
-  let apiKey = "d13aba718089eac946cbe226bfd205f4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
-  axios.get(apiUrl).then(showCurrentTemp);
-  console.log(latitude);
-  console.log(longitude);
-  console.log(apiUrl);
-}
-
-//Geolocation data is asked for when "current" button is clicked.
-function clickCurrentButton(event) {
-navigator.geolocation.getCurrentPosition(handlePosition);
-}
-
-//Function handles current temp showing.`
-function showCurrentTemp(response){
-  let temperature = Math.round(response.data.main.temp);
-  let humidity = response.data.main.humidity;
-  let weather = response.data.weather[0].description;
-  let city = response.data.name;
-  let changeTemp = document.querySelector("#temperature");
-  let changeHumidity = document.querySelector("#humidity");
-  let changeWeatherDescription = document.querySelector("#weather-description");
-  let changeCity = document.querySelector("#city-location");
-  changeCity.innerHTML = `${city}`;
-  changeTemp.innerHTML = `${temperature}`;
-  changeHumidity.innerHTML = `The Humidity is ${humidity}%`;
-  changeWeatherDescription.innerHTML = weather;
-}
-
-//Function handles current temp showing.`
+//Function will only run when the Submit button is hit. Using event listener on the form.
 function updateDisplay(response){
-  let temperature = Math.round(response.data.main.temp);
-  let humidity = response.data.main.humidity;
-  let weather = response.data.weather[0].description;
-  let changeTemp = document.querySelector("#temperature");
-  let changeHumidity = document.querySelector("#humidity");
-  let changeWeatherDescription = document.querySelector("#weather-description");
-  changeTemp.innerHTML = `${temperature}`;
-  changeHumidity.innerHTML = `The Humidity is ${humidity}%`;
-  changeWeatherDescription.innerHTML = weather;
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#day-hour");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;  
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000)
 }
 
 //Function uses the OpenWeatherAPI to look up the city.
 function getCity(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-text-input");
-  let changeCity = document.querySelector("#city-location");
+  let searchInput = document.querySelector("#search-input");
+  let changeCity = document.querySelector("#city");
   let units = "metric";
   let askCityName = searchInput.value;
   let apiKey = "d13aba718089eac946cbe226bfd205f4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${askCityName}&units=${units}&appid=${apiKey}`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(updateDisplay)
   if (searchInput.value) {
     changeCity.innerHTML = `${searchInput.value}`;
@@ -64,34 +32,70 @@ function getCity(event) {
   }
 }
 
-//Current button, clicking it calls functions.
-let currentButton = document.querySelector("#button-current-temp");
-currentButton.addEventListener("click", clickCurrentButton);
+//Function handlePosition asks for Geolocation data from the user and then feeds it to showCurrentTemp`
+function handlePosition(position){
+  let latitude= position.coords.latitude;
+  let longitude= position.coords.longitude;
+  let units = "metric";
+  let apiKey = "d13aba718089eac946cbe226bfd205f4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showCurrentTemp);
+}
 
-//Search engine, when searching for a city, display the city name on the page after the user submits form.
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", getCity);
+//Geolocation data is asked for when "current" button is clicked.
+function clickCurrentButton(event) {
+navigator.geolocation.getCurrentPosition(handlePosition);
+}
 
+//Function handles current temp showing.
+function showCurrentTemp(response){
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#day-hour");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;  
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000)
+}
 
-//Setting the time
-let currentTime = new Date();
-let days = [
+function minutes_with_leading_zeros(currentTime) {
+  //Function will add leading 0's to the current time so that the time looks normal if it's a single digit for the minute. 
+  return (currentTime.getMinutes() < 10 ? '0' : '') + currentTime.getMinutes();
+}
+
+function formatDate(timestamp){
+  //calculate the date
+   let days = [
   "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday"
-];
-let currentDay = days[currentTime.getDay()];
-let currentHour = currentTime.getHours();
-let currentMinutes = currentTime.getMinutes();
-let date = `${currentDay} ${currentHour}:${currentMinutes}`;
+  "Saturday"];
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = minutes_with_leading_zeros(date);
+  let day = days[date.getDay()];
 
-let BigDate = document.querySelector("#day-hour");
-BigDate.innerHTML = `${date}`;
+  return `${day} ${hours}:${minutes}`;
+}
+
+//Current button, clicking it calls functions.
+let currentButton = document.querySelector("#currentButton");
+currentButton.addEventListener("click", clickCurrentButton);
+
+//Search engine, when searching for a city, display the city name on the page after the user submits form.
+let form = document.querySelector("form");
+form.addEventListener("submit", getCity);
 
 
-
-
+let units = "metric"
+let apiKey = "d13aba718089eac946cbe226bfd205f4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Los Angeles&units=${units}&appid=${apiKey}`;
+axios.get(apiUrl).then(showCurrentTemp);
